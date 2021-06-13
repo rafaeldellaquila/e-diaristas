14f0886c8af9375180aad682417617f8a042ba0c
Rafael <rafadellaquila@gmail.com>
2021-06-12 23:45:51 -0300

    :recycle: adjust subtitle

--- src/ui/components/data-display/PageTitle/PageTitle.style.tsx
+++ src/ui/components/data-display/PageTitle/PageTitle.style.tsx
@@ -20,6 +20,7 @@ export const PageSubtitleStyled = styled('h3')`
   margin: ${({ theme }) => theme.spacing(1.5)} 0;
   color: ${({ theme }) => theme.palette.text.primary};
   font-size: ${({ theme }) => theme.typography.body1.fontSize};
+  font-weight: normal;
 
   ${({ theme }) => theme.breakpoints.down('md')} {
     font-size: ${({ theme }) => theme.typography.body2.fontSize};
